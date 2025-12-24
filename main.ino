0a23c06e99db3dba8a5616d0eaa321fef8bcbc1e
superroket169 <isagullu2010@gmail.com>
2025-12-24 20:02:09 +0300

    SideClock Class added

--- main.ino
+++ main.ino
@@ -1,19 +1,77 @@
+// main.ino
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-int saat = 0;
-int dak  = 1;
-int san  = 5;
+struct Time
+{
+    int8_t hour;
+    int8_t min;
+    int8_t sec;
+};
+// 1 saat = 3600 sn
+// 1 dk = 60 sn
+
+class SideClock
+{
+private:
+
+    int32_t totalMillisec = 0;
+    uint32_t beginTime = 0;
+
+public:
+
+    SideClock() = default;
+
+    int8_t getHour() const
+    {
+        return ((totalMillisec - (millis() - beginTime)) / (3600L * 1000L)) % 24L;
+    }
+
+    int8_t getMin() const
+    {
+        return ((totalMillisec - (millis() - beginTime)) / (60L * 1000L)) % 60L;
+    }
+
+    int8_t getSec() const
+    {
+        return ((totalMillisec - (millis() - beginTime)) / 1000L) % 60L;
+    }
+
+    void setClock(Time inpTime)
+    {
+        totalMillisec = 0;
+        totalMillisec = (inpTime.hour * 3600000L) + (inpTime.min * 60000L) + (inpTime.sec * 1000L);
+    }
+
+    // başlatır
+    void startClock()
+    {
+        beginTime = millis();
+    }
+
+    // saati durdurur ve sıfırlar
+    void stopClock()
+    {
+        beginTime = 0;
+    }
+
+    // yeniden başlatır. durdurmaz
+    void restartClock()
+    {
+        beginTime = millis();
+    }
+
+};
 
 char zaman[9]; // "HH:MM:SS"
 
 const int buttonPins[5] = {4, 5, 6, 7, 8};
 /*
-    4 -> menüden çıkıp başlatma butonu. start button
-    6 -> menüde dakika/saniye ayarlamak için (+)
-    7 -> menüde dakika/saniye ayarlamak için (-)
+    4 -> ayar değiştirme ve menüden çıkıp başlatma butonu. start button
+    6 -> menüde saat/dakika/saniye ayarlamak için (+)
+    7 -> menüde saat/dakika/saniye ayarlamak için (-)
     8 -> rakip kişiye geçme butonu 
     9 -> rakip kişiye geçme butonu
 */
@@ -21,54 +79,32 @@ const int buttonPins[5] = {4, 5, 6, 7, 8};
 
 const size_t numButtons = sizeof(buttonPins) / sizeof(buttonPins[0]);
 
-int inMenu = 1; // 0-1 
+int inMenu = 1; // 0-1
+
+SideClock whiteSide;
+SideClock blackSide;
 
 void setup()
 {
-    lcd.begin();
+    whiteSide.setClock((Time){0, 1, 5});
+    whiteSide.startClock();
+
+    lcd.init();
     lcd.backlight();
 
     for(int i = 0; i < numButtons; ++i)
-        pinMode(buttonPins[0], INPUT_PULLUP);
+        pinMode(buttonPins[i], INPUT_PULLUP);
 }
 
 void loop()
 {
-    sprintf(zaman, "%02d:%02d:%02d", saat, dak, san);
+    sprintf(zaman, "%02d:%02d:%02d", whiteSide.getHour(), whiteSide.getMin(), whiteSide.getSec());
     lcd.setCursor(0, 0);
     lcd.print(zaman);
 
-    if(buttonPins[0] == HIGH) inMenu = 0;
+    if(buttonPins[0] == LOW) inMenu = 0;
     
     if(inMenu) return;
-    
-    // main loop:
 
-    delay(1000);
     lcd.clear();
-    
-    if(san > 0) san--;
-    else
-    {
-        san = 59;
-        if(dak > 0) dak--;
-        else
-        {
-            dak = 59;
-            if (saat > 0) saat--;
-        }
-    }
-
-    // bitiş kontrolü
-    if(saat == 0 && dak == 0 && san == 0)
-    {
-        while(1) // errory finish
-        {
-            lcd.print(zaman);
-            delay(1000);
-            lcd.clear();
-            delay(500);
-        }
-    }
-
 }
